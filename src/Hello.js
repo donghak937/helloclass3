c93cd4b86c1826c0fb0c047a2d659b91344a0a87
dongha <donghak937@gmail.com>
2025-11-10 12:25:49 +0900

    add

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import './Hello.css';
+
+function Hello(props) {
+    return (
+            <h1 className = "Hello_h1">Hello!!!~~~@#!@#%$$#$%^&%^&*%^^&*&^%$#$%$^&*^%$!</h1>
+    );
+}
+
+export default Hello;
